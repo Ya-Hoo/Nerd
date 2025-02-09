46e86980d0bed555f7fb4e3851cb1b561964991c
YaHoo <huynhgiaphu3004@gmail.com>
2025-02-09 23:06:39 +0700

    changed title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Gia Phu's Brain",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
