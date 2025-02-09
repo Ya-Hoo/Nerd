322565aa0a94e9d805671e22ebfcb893958fb2cb
YaHoo <huynhgiaphu3004@gmail.com>
2025-02-09 23:14:35 +0700

    Quartz sync: Feb 9, 2025, 11:14 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Gia Phu's Brain",
+    pageTitle: "🦉 Nerd",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
